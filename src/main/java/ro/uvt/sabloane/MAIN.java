f95684238b3e9211603964515d8f81b1e25e99fc
NemethTimea <73704270+NemethTimea@users.noreply.github.com>
2021-10-11 20:06:42 +0300

    Lab2--Implementare diagrama

--- src/main/java/ro/uvt/sabloane/MAIN.java
+++ src/main/java/ro/uvt/sabloane/MAIN.java
@@ -1,15 +1,10 @@
 package ro.uvt.sabloane;
 
-import java.util.Collection;
-
 public class MAIN {
     public static void main(String[] args) {
         Book discoTitanic = new Book("Disco Titanic");
         Author rpGheo = new Author("Radu Pavel Gheo");
         discoTitanic.addAuthor(rpGheo);
-
-        Collection<Author> col_a = discoTitanic.getAuthors();
-        //col_a.add(new Author("Mircea Cartarescu"));
 
         int indexChapterOne = discoTitanic.createChapter("Capitolul 1");
         Chapter chp1 = discoTitanic.getChapter(indexChapterOne);
